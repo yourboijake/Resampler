2af5b7c5ac5ed3a49c7d0817042b4a042a1a0684
yourboijake <jakeydunning98@gmail.com>
2023-06-08 19:36:00 -0600

    implemented residual linearity test

--- heckman_correction.py
+++ heckman_correction.py
@@ -3,7 +3,7 @@ import numpy as np
 from statsmodels.discrete.discrete_model import Probit
 from statsmodels.tools import add_constant
 from sklearn.linear_model import LogisticRegression
-from scipy.stats import shapiro
+from scipy.stats import shapiro, linregress
 
 '''
 implement probit regression, returns array of probits given exogenous variables
@@ -32,10 +32,16 @@ def test_error_normality(x: np.array) -> tuple:
 
 #https://cros-legacy.ec.europa.eu/sites/default/files/WP_5_Meeting_2017_06_07-08_Madrid_Presentation_Statistics_Romania.pdf
 def test_error_independence(x: np.array, err: np.array):
+    #https://en.wikipedia.org/wiki/Hoeffding%27s_independence_test
+    
     pass
 
 def test_error_zero_mean(err: np.array):
     pass
+
+def test_residual_linearity(r1: np.array, r2: np.array):
+    lreg = linregress(r1, r2)
+    return lreg.rvalue, lreg.pvalue
 
 def heckman_bias_correction(X: np.array, y: np.array):
     #do probit regression
