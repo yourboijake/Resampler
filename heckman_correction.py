4e4ed3339c0873b659c00ef796291ddb1eabf504
yourboijake <jakeydunning98@gmail.com>
2023-06-08 19:17:26 -0600

    more heckman correction

--- heckman_correction.py
+++ heckman_correction.py
@@ -3,6 +3,7 @@ import numpy as np
 from statsmodels.discrete.discrete_model import Probit
 from statsmodels.tools import add_constant
 from sklearn.linear_model import LogisticRegression
+from scipy.stats import shapiro
 
 '''
 implement probit regression, returns array of probits given exogenous variables
@@ -26,8 +27,21 @@ def probit_regression(X: np.array, y: np.array) -> np.array:
     return y_probit
 
 #tests assumption in Heckman correction that errors are normal
-def test_error_normality(x: np.array):
+def test_error_normality(x: np.array) -> tuple:
+    return shapiro(x)
+
+#https://cros-legacy.ec.europa.eu/sites/default/files/WP_5_Meeting_2017_06_07-08_Madrid_Presentation_Statistics_Romania.pdf
+def test_error_independence(x: np.array, err: np.array):
+    pass
+
+def test_error_zero_mean(err: np.array):
     pass
 
 def heckman_bias_correction(X: np.array, y: np.array):
+    #do probit regression
+
+    #test for normality of errors
+
+    #
+
     pass
